91eb331bc3c1647bcab21d3df74cfc6d07664b79
Dário Jr <53487868+BirdRa1n@users.noreply.github.com>
2024-12-15 12:42:19 -0300

    Update server.ts
    configurando mqtt

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,17 @@
-console.log("Hello world!");
+import Aedes from 'aedes';
+import { createServer } from 'net';
+import { handleClient } from './mqtt/handlers/client';
+import { handleDisconnect } from './mqtt/handlers/disconnect';
+import { handlePublish } from './mqtt/handlers/publish';
+import { handleSubscriptions } from './mqtt/handlers/subscribe';
+
+
+const aedes = new Aedes();
+const server = createServer(aedes.handle);
+server.listen(1883, () => {
+    console.log(`MQTT Server 🚀 listening on port tcp: ${1883}`);
+    handleClient(aedes);
+    handleDisconnect(aedes);
+    handlePublish(aedes);
+    handleSubscriptions(aedes);
+});
