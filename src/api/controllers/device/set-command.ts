9177c15ca2f158a311a62208031bb6642d797a2e
Dário Jr <53487868+BirdRa1n@users.noreply.github.com>
2024-12-15 15:50:10 -0300

    Update set-command.ts
    Co-Authored-By: Iago Amorim <68704070+danonep2@users.noreply.github.com>

--- src/api/controllers/device/set-command.ts
+++ src/api/controllers/device/set-command.ts
@@ -1,24 +1,28 @@
 import { Request, Response, NextFunction } from "express"; // Importe Request e Response do Express
 import Command from "../../models/commands";
+import Device from "../../models/device";
 
-interface SetCommandInterface {
-    req: Request;
-    res: Response;
-    next: NextFunction
-}
-const SetCommand = async ({ req, res, next }: SetCommandInterface) => {
+import ControllerInterface from "../../interface/controller.interface"
+
+const SetCommand = async ({ req, res, next }: ControllerInterface) => {
     try {
         const { newCommand, id } = req.params;
+
+        const device = await Device.findOne({ where: { id } });
+
+        if (!device) {
+            throw ('Device not found')
+        }
 
         const command = await Command.create(
             { value: newCommand, device_id: id },
         );
 
-        res.send(`Command {${newCommand}} is updated.`);
+        res.json({ message: `Command is updated.`, newCommand });
 
     } catch (err) {
         console.log(err);
-        return res.status(404).json({ message: "Device not found" });
+        return res.status(404).json({ message: err });
     }
 }
 
