fb91cade1b77ce2ea2de71ea2a8cc3889b8664c7
Dário Jr <53487868+BirdRa1n@users.noreply.github.com>
2024-12-15 16:52:14 -0300

    Update disconnect.ts
    Co-Authored-By: Iago Amorim <68704070+danonep2@users.noreply.github.com>

--- src/mqtt/handlers/disconnect.ts
+++ src/mqtt/handlers/disconnect.ts
@@ -1,7 +1,9 @@
 import Aedes, { Client } from 'aedes';
+import Device from '../../api/models/device';
 
 export function handleDisconnect(aedes: Aedes) {
     aedes.on('clientDisconnect', async (client: Client) => {
         console.log(`Client disconnected: ${client.id}`);
+        await Device.update({ online: false }, { where: { cliente_id: client.id } });
     });
 }
