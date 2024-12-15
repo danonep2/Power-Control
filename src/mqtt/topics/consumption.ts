b39d985c1772fc95b37c4a631f35d4d5c686cbb8
Dário Jr <53487868+BirdRa1n@users.noreply.github.com>
2024-12-15 15:49:53 -0300

    Update consumption.ts
    Co-Authored-By: Iago Amorim <68704070+danonep2@users.noreply.github.com>

--- src/mqtt/topics/consumption.ts
+++ src/mqtt/topics/consumption.ts
@@ -1,5 +1,19 @@
 import Aedes, { Client } from "aedes";
+import Device from "../../api/models/device";
+import Consumption from "../../api/models/consumption";
 
-export function handleConsumption(aedes: Aedes, client: Client, payload: string) {
+export async function handleConsumption(aedes: Aedes, client: Client, payload: string) {
     console.log(`Consumption: ${payload}`);
+    const client_id = client.id;
+
+    const device = await Device.findOne({ where: { cliente_id: client_id } });
+    if (device) {
+        const consumption = payload;
+
+        //verificar se o valor pode ser consertido para float
+        const consumption_float = parseFloat(consumption);
+        if (!isNaN(consumption_float)) {
+            await Consumption.create({ value: consumption, device_id: device.id });
+        }
+    }
 }
