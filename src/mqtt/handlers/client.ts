e1d11cc521d1e7b1707bfd58927fab4da120ad99
Dário Jr <53487868+BirdRa1n@users.noreply.github.com>
2024-12-15 15:49:55 -0300

    Update client.ts
    Co-Authored-By: Iago Amorim <68704070+danonep2@users.noreply.github.com>

--- src/mqtt/handlers/client.ts
+++ src/mqtt/handlers/client.ts
@@ -1,7 +1,15 @@
 import Aedes, { Client } from 'aedes';
+import Device from '../../api/models/device';
 
 export async function handleClient(aedes: Aedes) {
-    aedes.on('client', (client: Client) => {
+    aedes.on('client', async (client: Client) => {
         console.log(`Client ${client.id} connected`);
+        const client_id = client.id;
+
+        await Device.findOne({ where: { cliente_id: client_id } }).then((device) => {
+            if (!device) {
+                Device.create({ tag: client_id, cliente_id: client_id });
+            }
+        });
     });
 }
