e54484faa435cf3d63f16a48f44f2a40d6e7616b
Dário Jr <53487868+BirdRa1n@users.noreply.github.com>
2024-12-15 14:29:20 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,7 @@ import sequelize from './api/config/sequelize';
 import User from './api/models/user';
 
 import bcrypt from 'bcrypt';
+import Auth from './api/middleware/auth';
 
 
 // MQTT
@@ -32,6 +33,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+    const publicRoutes = ['/api/v1/register'];
+    if (publicRoutes.includes(req.path)) {
+        return next();
+    }
+    Auth(req, res, next);
+});
+
 app.use("/api/v1", router);
 
 app.listen(80, () => {
@@ -43,7 +52,6 @@ app.listen(80, () => {
         await sequelize.authenticate();
         await sequelize.sync({ alter: true });
 
-        //await sequelize.sync({ alter: true });
         console.log("Conexão ao banco de dados estabelecida com sucesso.");
 
         //verificar se o usuario admin existe
@@ -55,6 +63,7 @@ app.listen(80, () => {
                 auth_hash: await bcrypt.hash(process.env.ADMIN_PASSWORD || 'admin', 10)
             });
         }
+
     } catch (error) {
         console.error("Não foi possível conectar ao banco de dados:", error);
     }
